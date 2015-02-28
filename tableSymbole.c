0c0a439d864fc82f26ded1f7da6f4da6ecf6b83b
Name-less <ph0enix.fiire@gmail.com>
2015-02-28 20:08:34 +0100

    implementation of the table of symbole without any test

--- tableSymbole.c
+++ tableSymbole.c
@@ -1,1 +1,58 @@
-Our c file
+#include <stdlib.h>
+#include <tableSymbole.h>
+
+struct Symbole {
+	char * type;
+	char * name;
+	int * adress;
+	Symbole * next;
+	Symbole * before;
+}symbole;
+
+Symbole * lastOne;
+int ts_init(){
+	lastOne = (Symbole *)malloc(sizeof(struct Symbole));
+	if(lastOne != NULL){
+		return 1;
+	}else{
+		return -1;
+	}
+}
+
+int ts_create(char * name,char * type){
+	if(exist(name) == 1){
+		Symbole * newSymbole = (Symbole *)malloc(sizeof(struct Symbole));
+		newSymbole.name = name;
+		newSymbole.type = type;
+		newSymbole.before = lastOne;
+		lastOne.next = newSymbole;
+		lastOne = new Symbole;
+		return 1;
+	}else{
+		return -1;
+	}
+}
+
+int exist(char * name){
+	Symbole * iterator = lastOne;
+	while(iterator.before != NULL){
+		if(*iterator.name == *name){
+			return -1;
+		}else{
+			iterator = lastOne.before;
+		}
+	}
+	return 1;
+}
+
+int ts_delete(char * name){
+        Symbole * iterator = lastOne;
+        while(iterator.before != NULL){
+                if(*iterator.name == *name){
+                        iterator.before.next = iterator.next;
+                }else{
+                        iterator = lastOne.before;
+                }
+        }
+        return 1;
+}
--- tableSymbole.h
+++ tableSymbole.h
@@ -1,1 +1,4 @@
-Our h point
+int ts_create(char * name,char type);
+int ts_delete(char * name);
+int ts_init();
+int exist(char * name);
