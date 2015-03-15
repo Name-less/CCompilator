78c22b926933ecde02a71b4510521714c4360286
Name-less <ph0enix.fiire@gmail.com>
2015-03-15 17:50:52 +0100

    add every function to put instruction in the stack : MOV ADD SUB DIV MUL JUMP

--- instructions.c
+++ instructions.c
@@ -9,6 +9,7 @@ par le yacc
 On ne différencie pas les variables temporaires des variables de la table des symboles
 
 */
+
 struct Stack_Instruction{
 	int [3] instruct_params;
 	stack_inst * next_instruct;
@@ -16,21 +17,29 @@ struct Stack_Instruction{
 }stack_inst;
 
 stack_inst * first_stack;
-void stack_init(int cop,int arg1,int arg2){
+void stack_init(stack_inst * inst){
 	first_stack = malloc(sizeof(stack_inst));
 	if(first_stack != NULL){
 		first_stack->instruct_params[0] = cop;
 		first_stack->instruct_params[1] = arg1;
 		first_stack->instruct_params[2] = arg2;
 		first_stack->next_instruct = NULL;
-		first_stack->before_instruct = NULL;
+		first_stack->before_instruct = first_stack;
 	}else{
 		printf("Fail to init stack instruction");
 	}
 }
 
+/*
+push any instructions to the stack
+*/
+
 void stack_push(stack_inst * inst_to_add){
 	stack_inst * iterator = first_stack;
+	if(first_stack->before_instruct == NULL){
+		stack_init(inst_to_add);
+		return;
+	}
 	while(iterator->next_instruct != NULL){
 		iterator = iterator->next_instruct;
 	}
@@ -38,33 +47,100 @@ void stack_push(stack_inst * inst_to_add){
 	iterator->next_instruct = inst_to_add;
 }
 
-void stack_push_add(int arg1, int arg2){
-	stack_inst * new_inst = malloc(sizeof(stack_inst));
-	new_inst->instruct_params[0]=INST_ADD;
-	new_inst->instruct_params[1]=arg1;
-	new_inst->instruct_params[2]=arg2;
+/*
+pop the first instruction on the stack (lifo)
+*/
+
+stack_inst * stack_pop(){
+	if(first_stack->before_instruct != NULL){
+		stack_inst * iterator = first_stack;
+		while(iterator->next_instruct != NULL){
+			iterator = iterator->next_instruct;
+		}
+		return iterator;
+	}else{
+		return NULL;
+	}
 }
 
-
-
-void print_assembler_instructions(char * operation, char * op1,char * op2,char * to_print){
+void print_assembler_instructions(stack_inst * inst,char * to_print){
 
    FILE * fp;
 
    fp = fopen (to_print, "a+");
 
-	if(strcmp(operation,"+") == 0){
-		fprintf(fp,"ADD %s %s",op1,op2);
-	}else if(strcmp(operation,"-") == 0){
-                fprintf(fp,"SUB %s %s",op1,op2);
-	}else if(strcmp(operation,"*") == 0){
-                fprintf(fp,"MUL %s %s",op1,op2);
-	}else if(strcmp(operation,"/") == 0){
-                fprintf(fp,"DIV %s %s",op1,op2);
-	}
+	if(INST_ADD == inst->instruct_params[0]){
+		fprintf(fp,"ADD %s %s",inst->instruct_params[1],inst->instruct_params[2]);
+	else if(INST_MUL == inst->instruct_params[0]){
+		fprintf(fp,MUL %s %s",inst->instruct_params[1],inst->instruct_params[2]);
+	else if(INST_DIV == inst->instruct_params[0]){
+		fprintf(fp,"DIV %s %s",inst->instruct_params[1],inst->instruct_params[2]);
+	}else if(INST_SUB == inst->instruct_params[0]){
+                fprintf(fp,"SUB %s %s",inst->instruct_params[1],inst->instruct_params[2]);
+        }else if(INST_JUMP =        else if(INST_DIV == inst->instruct_params[0]){
+                fprintf(fp,"DIV %s %s",inst->instruct_params[1],inst->instruct_params[2]);
+        }else if(INST_MOV == inst->instruct_params[0]){
+                fprintf(fp,"MOV %s %s",inst->instruct_params[1],inst->instruct_params[2]);
+        }
    
    	fclose(fp);
 }
+
+/*
+Our function to store instructions in the stack
+*/
+
+void stack_push_add(int arg1, int arg2){
+        stack_inst * new_inst = malloc(sizeof(stack_inst));
+        new_inst->instruct_params[0]=INST_ADD;
+        new_inst->instruct_params[1]=arg1;
+        new_inst->instruct_params[2]=arg2;
+        stack_push(new_inst);
+}
+
+void stack_push_mov(int arg1, int arg2){
+        stack_inst * new_inst = malloc(sizeof(stack_inst));
+        new_inst->instruct_params[0]=INST_MOVE;
+        new_inst->instruct_params[1]=arg1;
+        new_inst->instruct_params[2]=arg2;
+        stack_push(new_inst);
+}
+
+void stack_push_sub(int arg1, int arg2){
+        stack_inst * new_inst = malloc(sizeof(stack_inst));
+        new_inst->instruct_params[0]=INST_SUB;
+        new_inst->instruct_params[1]=arg1;
+        new_inst->instruct_params[2]=arg2;
+        stack_push(new_inst);
+}
+
+void stack_push_mul(int arg1, int arg2){
+        stack_inst * new_inst = malloc(sizeof(stack_inst));
+        new_inst->instruct_params[0]=INST_MUL;
+        new_inst->instruct_params[1]=arg1;
+        new_inst->instruct_params[2]=arg2;
+        stack_push(new_inst);
+}
+
+void stack_push_div(int arg1, int arg2){
+        stack_inst * new_inst = malloc(sizeof(stack_inst));
+        new_inst->instruct_params[0]=INST_DIV;
+        new_inst->instruct_params[1]=arg1;
+        new_inst->instruct_params[2]=arg2;
+        stack_push(new_inst);
+}
+
+void stack_push_jump(int arg1, int arg2){
+        stack_inst * new_inst = malloc(sizeof(stack_inst));
+        new_inst->instruct_params[0]=INST_JUMP;
+        new_inst->instruct_params[1]=arg1;
+        new_inst->instruct_params[2]=arg2;
+        stack_push(new_inst);
+}
+
+/*
+End of our functions to add instructions to the stack
+*/
 
 int main(){
 	print_assembler_instructions((char *)"+",(char *)"a",(char *)"b",(char*)"toto");
--- instructions.h
+++ instructions.h
@@ -1,5 +1,10 @@
 
 #define INST_ADD;
+#define INST_MOV;
+#define INST_SUB;
+#define INST_MUL;
+#define INST_DIV;
+#define INST_JUMP;
 
 void stack_init(int cop,int arg1,int arg2);
 
